83a7eb2d519c9f60928cf0eb7f9e716d6ed4980c
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2019-10-19 00:58:10 +0700

    add webhook

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,20 +1,20 @@
 # Import installed packages
 from flask import Flask, jsonify, request
-i
+
 # Import app code
 app = Flask(__name__)
 
 # After creating the app, so that cors can import it
 from app import cors
 
+FB_API_URL = 'https://graph.facebook.com/v4.0/me/messages'
+VERIFY_TOKEN = "test12345678"
+PAGE_ACCESS_TOKEN = "EAAjjuJJnoEgBAB9f87NlpokL0aZAdbrX9JYECKVbAcJZCQrZBo2cojpQCzY68h7ZAV2HzfuHOEIvrOCMhYLqjesefPaEazag6IKnPJZChs75eYZAc8MSBbhmZCk9ct3ZC6yjZCz006AMhJgv3iMFYtfXUmOm3rYpfxf9rOtwZBSp7SJuGGIlFqmUUnW7P5iMDYZB2gZD"
 
 @app.route("/api/")
 def root():
     return jsonify({"message": "Hello World"})
 
-FB_API_URL = 'https://graph.facebook.com/v4.0/me/messages'
-VERIFY_TOKEN = "test12345678"
-PAGE_ACCESS_TOKEN = "EAAjjuJJnoEgBAB9f87NlpokL0aZAdbrX9JYECKVbAcJZCQrZBo2cojpQCzY68h7ZAV2HzfuHOEIvrOCMhYLqjesefPaEazag6IKnPJZChs75eYZAc8MSBbhmZCk9ct3ZC6yjZCz006AMhJgv3iMFYtfXUmOm3rYpfxf9rOtwZBSp7SJuGGIlFqmUUnW7P5iMDYZB2gZD"
 
 
 def get_bot_response(message):
@@ -50,6 +50,6 @@ def listen():
     listen at the `/webhook` endpoint"""
     if request.method == 'GET':
         return verify_webhook(request)
-
+#
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
